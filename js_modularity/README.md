f76687d7549be28e8b1949b2217e363151d4c944
Yuriy Husnay <husnaj23@gmail.com>
2013-11-12 21:33:51 +0200

    initial commit of Modularity in JS

---
+++ js_modularity/README.md
@@ -0,0 +1,8 @@
+
+## JavaScript Style Guide
+#### Yuriy Husnay
+
+
+-------
+
+slideshow template based on [Google I/O 2012 slides](https://code.google.com/p/io-2012-slides/):
---
+++ js_modularity/slide_config.js
@@ -0,0 +1,30 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Modularity in JS',
+    subtitle: 'Modules, AMD, CJS, Harmony',
+    eventInfo: {
+     title: 'Front-End community meeting',
+     date: '14/11/2013'
+    },
+    useBuilds: true,
+    usePrettify: true,
+    enableSlideAreas: false,
+    enableTouch: false,
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Yuriy Husnay',
+    company: 'Eleks',
+    gplus: 'https://plus.google.com/112258156152431356191',
+    twitter: '@YuriyHusnay',
+    github: 'http://github.com/husa'
+  }]
+};
+
